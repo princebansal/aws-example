c27bd5c01a05cff6a8dbbc4ad4f0a73dd1e3b1fc
Prince Bansal <princebansal@users.noreply.github.com>
2016-12-10 00:39:44 +0530

    Updated
    Should work.

--- MainActivity.java
+++ MainActivity.java
@@ -30,63 +30,67 @@ import static android.R.id.input;
 
 public class MainActivity extends AppCompatActivity {
     int hours = new Time(System.currentTimeMillis()).getHours();
-
-
-
-
-
-
-
-    public EditText NapTime=(EditText) findViewById(R.id.nap_time);
-    public EditText FeedTime=(EditText) findViewById(R.id.feed_time);
-
     public Context applicationContext;
-    CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
-            applicationContext, // get the context for the current activity
-            "us-west-2_zG713i4td", // Identity Pool ID
-            Regions.US_WEST_2 //Region
-    );
-    AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+    CognitoCachingCredentialsProvider credentialsProvider;
+    AmazonDynamoDBClient ddbClient;
 
-    String identityId=credentialsProvider.getIdentityId();
-   public int func()
-    {
-        Log.d("LogTag", "my ID is " + identityId);
-        return 0;
-    }
-    int logged=func();
+    String identityId;
+  
+    int logged;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        public EditText NapTime=(EditText) findViewById(R.id.nap_time);
+        public EditText FeedTime=(EditText) findViewById(R.id.feed_time);
         Button Button1=(Button) findViewById(R.id.button1);
+        
+        logged=func();
+        
+        credentialsProvider = new CognitoCachingCredentialsProvider(
+            applicationContext, // get the context for the current activity
+            "us-west-2_zG713i4td", // Identity Pool ID
+            Regions.US_WEST_2 //Region
+        );
+        ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+        identityId=credentialsProvider.getIdentityId();
+        
         Button1.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View view)
             {
+                if(!TextUtils.isEmpty(NapTime.getText().toString)&&!TextUtils.isEmpty(FeedTime.getText().toString)){
+                    int NapTime1= Integer.parseInt(NapTime.getText().toString());
+                    int FeedTime1= Integer.parseInt(FeedTime.getText().toString());
+                    if((NapTime1-hours)==1)
+                    {
+                        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                        String MyText = "Time to put the baby to sleep";
+                        Notification mNotification = new Notification(R.drawable.notification_icon, MyText, System.currentTimeMillis() );
 
-                int NapTime1= Integer.parseInt(NapTime.getText().toString());
-                int FeedTime1= Integer.parseInt(FeedTime.getText().toString());
-                if((NapTime1-hours)==1)
-                {
-                    NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                    String MyText = "Time to put the baby to sleep";
-                    Notification mNotification = new Notification(R.drawable.notification_icon, MyText, System.currentTimeMillis() );
+                    }
+                    if((FeedTime1-hours)==1)
+                    {
+                        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                        String MyText1 = "Time to feed the baby";
+                        Notification mNotification = new Notification(R.drawable.notification_icon, MyText1, System.currentTimeMillis() );
 
-                }
-                if((FeedTime1-hours)==1)
-                {
-                    NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                    String MyText1 = "Time to feed the baby";
-                    Notification mNotification = new Notification(R.drawable.notification_icon, MyText1, System.currentTimeMillis() );
-
+                    }
                 }
 
             }
         });
 
+    }
+    
+     public int func()
+    {
+       if(identityId!=null){
+           Log.d("LogTag", "my ID is " + identityId);
+       }
+       return 0;
     }
 
 }
